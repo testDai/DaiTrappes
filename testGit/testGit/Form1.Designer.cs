3b6344b48c1525969e238605af9cd1f6b89103bf
Amat <Amat@DESKTOP-UDOBVH0>
2016-11-16 12:14:10 +0100

    New background color !

--- testGit/testGit/Form1.Designer.cs
+++ testGit/testGit/Form1.Designer.cs
@@ -75,6 +75,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.btValider);
             this.Controls.Add(this.lbApple);
