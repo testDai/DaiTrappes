333b87586f11a62a7d4837e86bb051f44ff855c4
VERONIQUE-A <VERONIQUE-A@VERONIQUE>
2016-11-16 12:38:56 +0100

    j'ai rajouter une fenetre

---
+++ testGit/testGit/FenetreTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testGit
+{
+    public partial class FenetreTest : Form
+    {
+        public FenetreTest()
+        {
+            InitializeComponent();
+        }
+    }
+}
